45c904250f8d22a905c93ed65f0222afca522d45
marcos <markusvynycyus@hotmail.com>
2022-02-03 12:03:22 -0300

    Alterando a Tabela Restaurante, adicionado o relacionamento MuitosParaMuitos, com a tabela Produto.

--- src/main/java/com/venicios/delivery/model/Restaurante.java
+++ src/main/java/com/venicios/delivery/model/Restaurante.java
@@ -54,4 +54,8 @@ public class Restaurante {
                joinColumns = @JoinColumn(name= "restaurante_id"),
                inverseJoinColumns = @JoinColumn(name ="forma_pagamento_id"))
     private List<FormaPagamento> formasPagamento = new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "restaurante")
+    private List<Produto> produtos = new ArrayList<>();
 }
