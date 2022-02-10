5e4fd5d0790169fae6499dbdcf5a59e674f29cf2
marcos <markusvynycyus@hotmail.com>
2022-02-10 12:02:43 -0300

    Trabalhando com JPQL em RestauranteRepository

--- src/main/java/com/venicios/delivery/domain/repository/RestauranteRepository.java
+++ src/main/java/com/venicios/delivery/domain/repository/RestauranteRepository.java
@@ -2,12 +2,16 @@ package com.venicios.delivery.domain.repository;
 
 import com.venicios.delivery.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
+    @Query("from Restaurante where nome like %:nome and cozinha.id = :id")
     List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 
 }
